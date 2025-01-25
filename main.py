65bbc98e8365e445b14af5e5fa4f407a952087cd
Malaysia Thomas <105211177+MalaysiaThomas@users.noreply.github.com>
2025-01-25 20:18:57 +0000

    display weather for each city

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ with open("temperatures.csv", "r") as file:
     temp_file = csv.reader(file)
 
     for row in temp_file:
-        print(row)
+        print(f"It is currently {row[1]} degrees and {row[2].lower()} in {row[0]}.")
